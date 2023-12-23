6f6fdf225d2b3a111abcc858dd57d8e99b8a7c4e
unaio <unaio@LaptopUnai>
2023-12-23 13:58:16 +0100

    estos commits no valen

--- ProyectoZooYarzabal/src/Ventanas/VentanaVisitante.java
+++ ProyectoZooYarzabal/src/Ventanas/VentanaVisitante.java
@@ -111,7 +111,7 @@ public class VentanaVisitante extends JFrame{
         
         panelVideoyBotones.add(panelBotonesVideo, BorderLayout.SOUTH);
         
-   
+        //
 		
         JPanel pnlTexto = new JPanel(new GridBagLayout());
         // Agregaremos algunas condiciones para que luego el JTextArea se vea mejor
